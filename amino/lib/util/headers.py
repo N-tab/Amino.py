b4e0c9de15a03efeafd0ca6f6264c7702de33179
N-tab <65714812+N-tab@users.noreply.github.com>
2021-08-30 19:33:43 +0300

    Update headers.py

--- amino/lib/util/headers.py
+++ amino/lib/util/headers.py
@@ -3,25 +3,20 @@ from amino.lib.util import device
 sid = None
 
 class Headers:
-    def __init__(self, data = None, type = None, deviceId: str = None, sig: str = None):
-        if deviceId:
-            dev = device.DeviceGenerator(deviceId=deviceId)
-        else:
-            dev = device.DeviceGenerator()
-
+    def __init__(self, dev = device.DeviceGenerator(),data = None, type = None, sid = None):
         headers = {
             "NDCDEVICEID": dev.device_id,
-            "NDC-MSG-SIG": "Aa0ZDPOEgjt1EhyVYyZ5FgSZSqJt",
-            "Accept-Language": "en-US",
+            "NDCLANG": "ru",
+            "Accept-Language": "ru-RU",
+            "SMDEVICEID": "6e28d4c5-2d25-4977-93ec-9a4ce077fb7b",
             "Content-Type": "application/json; charset=utf-8",
-            "User-Agent": dev.user_agent,
+            "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 9; Redmi Note 8 Build/PKQ1.190616.001; com.narvii.amino.master/3.4.33578)",
             "Host": "service.narvii.com",
-            "Accept-Encoding": "gzip",
-            "Connection": "Keep-Alive"
+            "Connection": "Keep-Alive",
+            "Accept-Encoding": "gzip"
         }
 
         if data: headers["Content-Length"] = str(len(data))
-        if sid: headers["NDCAUTH"] = f"sid={sid}"
+        if sid: headers["NDCAUTH"] = sid
         if type: headers["Content-Type"] = type
-        if sig: headers["NDC-MSG-SIG"] = sig
         self.headers = headers
